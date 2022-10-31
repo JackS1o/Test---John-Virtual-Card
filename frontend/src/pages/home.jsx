f098525d19e6e6959a6f0c035bd2d50c4243c9ed
Jackson <jviana116@gmail.com>
2022-10-31 09:17:39 -0300

    feat: adicionando estilo a pagina home

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,7 +1,11 @@
-import { useContext } from "react";
+import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import Context from "../Context/context";
 import createUser from "../api/createUser";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import "../styles/home.css";
 
 function Home() {
   const { userData, setUserData } = useContext(Context);
@@ -18,55 +22,56 @@ function Home() {
 
   const sendData = async () => {
     const result = await createUser(userData);
-    if (result.message === 'Usuário criado com sucesso') {
+    if (result.message === "Usuário criado com sucesso") {
       alert("User created successfully");
       navigate("/qrcode");
     }
-  }
+  };
 
   return (
-    <div>
-      <header>
+    <div className="main">
+      <header className="header">
         <h1>QR Code Generator</h1>
       </header>
-      <div>
-        <label htmlFor="url">
-          Name
-          <input
-            type="text"
-            placeholder="Type your First Name"
-            name="name"
-            value={userData.name}
-            onChange={dataInput}
-          />
-        </label>
-        <label htmlFor="url">
-          LinkedIn URL
-          <input
-            type="text"
-            placeholder="Type your LinkedIn URL"
-            name="linkedin"
-            value={userData.linkedin}
-            onChange={dataInput}
-          />
-        </label>
-        <label htmlFor="url">
-          GitHub URL
-          <input
-            type="text"
-            placeholder="Type your GitHub URL"
-            name="github"
-            value={userData.github}
-            onChange={dataInput}
-          />
-        </label>
-        <button
-          type="button"
-          onClick={sendData}
-        >
-          Generate QR Code
-        </button>
-      </div>
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "25ch" },
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          id="standard-basic"
+          label="Name"
+          variant="standard"
+          name="name"
+          value={userData.name}
+          onChange={dataInput}
+        />
+        <TextField
+          id="standard-basic"
+          label="LinkedIn URL"
+          variant="standard"
+          name="linkedin"
+          value={userData.linkedin}
+          onChange={dataInput}
+        />
+        <TextField
+          id="standard-basic"
+          label="GitHub URL"
+          variant="standard"
+          name="github"
+          value={userData.github}
+          onChange={dataInput}
+        />
+        <Button variant="contained" onClick={sendData}>
+          Link
+        </Button>
+      </Box>
     </div>
   );
 }
